8be871fb53c5b0e8ee4bf0f5995558511f82ba41
Chinmay <chinmayrpatil123@gmail.com>
2025-11-02 00:14:45 +0530

    Railway Deployment

--- db/pool.js
+++ db/pool.js
@@ -5,12 +5,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    },
-    host: process.env.DB_HOST || 'localhost', 
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    }
 });
 
 module.exports = pool;
